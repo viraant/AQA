0b2d800b71f1633c53001f0e15688194c71f34c6
Vira Antonova <viraantonova@Viras-MacBook-Air.local>
2025-09-26 23:27:00 +0300

    added decision tree

---
+++ lesson_3/decision-tree.js
@@ -0,0 +1,10 @@
+let message
+
+if (typeof message == "string") {
+    console.log("Thank you for filling the form")
+}
+else if (typeof message === 'number' || message === null) {console.log("Please write your message")}
+else {console.log("Please write your message")}
+
+message = null
+console.log(message)
