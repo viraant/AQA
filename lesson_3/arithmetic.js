8970118e2baa1a4744c6746bfa58baba4aedf68a
Vira Antonova <viraantonova@Viras-MacBook-Air.local>
2025-09-26 21:38:42 +0300

    added lesson 3

---
+++ lesson_3/arithmetic.js
@@ -0,0 +1,20 @@
+let a = 10;
+let e = 'test';
+let g = a+e;
+console.log(g);
+console.log(typeof g);
+
+let b = false;
+let j = b+a;
+let c = NaN;
+
+let d = '222';
+let l = d-a;
+console.log(typeof l);
+
+let k = d/a;
+console.log(k);
+console.log(typeof k);
+
+let m = a*e;
+console.log(m); 
---
+++ lesson_3/logic.js
@@ -0,0 +1,9 @@
+let abc=15;
+let outText= "Right";
+let isRight=((abc>10||abc==10)&&(abc<20 || abc==20));
+
+if(isRight){
+console.log(outText);
+}else {
+console.log("Wrong");
+};
