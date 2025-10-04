2e9b8d013a7bdabf8c4f98a55afb24953ba8b30e
Vira Antonova <viraantonova@Viras-MacBook-Air.local>
2025-10-04 13:42:05 +0300

    added changes

--- js/loop.js
+++ js/loop.js
@@ -1,10 +1,7 @@
-
-
-for (let i = 0; i<10; i=i+1) {
+for (let i=0; i < 10; i = i + 1) {
     console.log(i)
 };
 
-
-for (let i = 100; i>-1; i=i-10) {
+for (let i = 100; i >- 1; i = i-10) {
     console.log(i)
 };
